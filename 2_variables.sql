534821e678b450cc63043a2fe4e88d255fc94849
BishalSoftware <31771842+BishalSoftware@users.noreply.github.com>
2020-08-22 18:32:58 +0545

    Add files via upload

---
+++ 2_variables.sql
@@ -0,0 +1,17 @@
+--ANONYMOUS BLOCK
+
+SET SERVEROUTPUT ON;
+DECLARE
+	--v_test	VARCHAR2(15);
+	
+	--v_test VARCHAR2(15) := 'SoftwareRider';
+	
+	v_test VARCHAR2(15);
+BEGIN
+	v_test:='SoftwareRider';
+	DBMS_OUTPUT.PUT_LINE(v_test);
+END;
+	
+
+	
+	
